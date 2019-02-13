7f5ef3e21782ed16e713c71af2c5b00afc365446
Seb Bacon <seb.bacon@gmail.com>
2019-02-13 17:52:33 +0000

    Reinstate `finally` clause

--- ebmbot/openprescribing.py
+++ ebmbot/openprescribing.py
@@ -144,11 +144,12 @@ def deploy_timer(message):
                 message.reply(
                     "Error during deploy: {}".format(e), in_thread=True)
                 raise
-            if flags.deploy_queued:
-                flags.deploy_queued = False
-                deploy_live_delayed(message)
-            else:
-                flags.deploy_countdown = None
+            finally:
+                if flags.deploy_queued:
+                    flags.deploy_queued = False
+                    deploy_live_delayed(message)
+                else:
+                    flags.deploy_countdown = None
         else:
             sleep(1)
             if flags.deploy_countdown is not None:
