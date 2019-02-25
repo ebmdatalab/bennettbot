d34de2be0a4f75fa6809468ea8c7796214182afc
Seb Bacon <seb.bacon@gmail.com>
2019-02-25 14:01:51 +0000

    Work around fabric not passing Travis environment to subshells

--- tests/fabfile_example.py
+++ tests/fabfile_example.py
@@ -1,6 +1,7 @@
 from fabric.api import run
 from fabric.api import abort
 from fabric.api import env
+from fabric.context_managers import prefix
 
 env.user = None
 env.hosts = ['localhost']
@@ -9,12 +10,25 @@ env.colorize_errors = False
 env.disable_known_hosts = True
 
 
+def _remove_job_stages():
+    # A hack to address the fact that our Travis environment is not
+    # passed to the ssh subshell.
+    #
+    # Discussion of the issue here:
+    # https://travis-ci.community/t/travis-functions-no-such-file-or-directory/2286/2
+    run('if [ -f /home/travis/.travis/job_stages ];'
+        '  then echo "#!/bin/bash" > /home/travis/.travis/job_stages;'
+        'fi')
+
+
 def do_run():
-    return run('/bin/echo "hello world"')
+    _remove_job_stages()
+    return run('echo "hello world"')
 
 
 def do_disallowed_thing():
-    return run('/bin/cat /etc/sudoers')
+    _remove_job_stages()
+    return run('cat /etc/sudoers')
 
 
 def do_abort():
