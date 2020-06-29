4888818304c5b95208d6347d5ce946db5241d320
Peter Inglesby <peter.inglesby@gmail.com>
2020-06-29 09:45:08 +0100

    Move webserver main code to __main__.py

--- ebmbot/webserver/__main__.py
+++ ebmbot/webserver/__main__.py
@@ -1,1 +1,12 @@
-# So we can run package as a script
+from urllib.parse import urlparse
+
+from . import app
+from ..logger import logger
+from .. import settings
+
+
+logger.info("running ebmbot.webserver")
+port = urlparse(settings.WEBHOOK_ORIGIN).port
+app.run(
+    host="0.0.0.0", port=port, load_dotenv=False, debug=False,
+)
