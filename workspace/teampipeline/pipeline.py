33e68ef15f4399ff0fa0eb66e8997855500dd923
Lucy Bridges <lucy.bridges@thedatalab.org>
2022-11-09 14:59:00 +0000

    Add 'In Review' column for pipeline team report

--- workspace/teampipeline/pipeline.py
+++ workspace/teampipeline/pipeline.py
@@ -3,5 +3,5 @@ from workspace import generate_report
 
 def report():
     project_num = 12
-    statuses = ["In Progress", "Blocked"]
+    statuses = ["In Progress", "In Review", "Blocked"]
     return generate_report.main(project_num, statuses)
