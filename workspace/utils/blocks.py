e0c24965a8fecc4bfe733588fbc410ebf621b6b3
Alice Wong <alice.wong@thedatalab.org>
2024-09-17 21:59:27 +0100

    fix docstring

--- workspace/utils/blocks.py
+++ workspace/utils/blocks.py
@@ -19,7 +19,7 @@ def get_basic_header_and_text_blocks(
     Returns a list of blocks: a header block and text blocks with little room for customization.
     "texts" can be a list of strings or a single string; In the former case, each string will be a separate text block.
     The header block is a plain text header block.
-    The text blocks are markdown block of block type "section".
+    The text blocks have block type "section" and the text type is "mrkdwn" by default.
     """
     header_block = get_header_block(header_text)
     if isinstance(texts, str):
