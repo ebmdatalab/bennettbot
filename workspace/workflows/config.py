2a1cfa980da24108000d2f823032daa9226f243a
Ben Butler-Cole <ben@bridesmere.com>
2024-10-15 11:50:39 +0100

    Minor whitespace fix

--- workspace/workflows/config.py
+++ workspace/workflows/config.py
@@ -112,7 +112,7 @@ SKIPPED_WORKFLOWS_ON_MAIN = {
         26915901,  # [On workflow call] Add software bill of materials to release (reusable)
         26915902,  # [On workflow call] Scan with Grype (reusable)
         25002877,  # [On PR] Dependency review
-        2393224,  #  [On PR] Tests
+        2393224,  # [On PR] Tests
     ],
     "opensafely-core/pipeline": [
         77090712,  # [Disabled] Pin pydantic
@@ -128,6 +128,6 @@ SKIPPED_WORKFLOWS_ON_MAIN = {
         13520184,  # [On workflow dispatch] Check Trino version on EMIS and in docker-compose.yml match
     ],
     "ebmdatalab/bennettbot": [
-        32719413,  #  Auto merge Dependabot PRs
+        32719413,  # Auto merge Dependabot PRs
     ],
 }
