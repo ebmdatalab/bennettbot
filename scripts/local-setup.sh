83d7f5f5d5361a680697fc7d31b0cae68baa03cd
Iain Dillingham <iain@dillingham.me.uk>
2024-12-05 15:25:47 +0000

    Identify Codespaces at risk of deletion
    Adds a `codespaces at risk` command to identify researchers whose
    Codespaces are at risk of deletion. The `org` and `threshold_in_days`
    variables aren't parametrised, but could be should different groups of
    users have different requirements.
    
    Closes opensafely-core/research-template-docker#70

--- scripts/local-setup.sh
+++ scripts/local-setup.sh
@@ -71,6 +71,7 @@ elif test "$SLACK_BOT_TOKEN" = "changeme" -o -z "$SLACK_BOT_TOKEN" -o "$FORCE_UP
     SLACK_APP_TOKEN_BW_ID=3738619b-4e7d-4932-84b0-b1e700942908
     SLACK_SIGNING_SECRET_BW_ID=946080f1-c50f-4edf-9773-b1e70095747a
     DATA_TEAM_GITHUB_API_TOKEN_BW_ID=3c8ca5df-2fa1-49ac-afd6-b1e70092fd2a
+    CODESPACES_GITHUB_API_TOKEN_BW_ID=4d64924a-18c5-48a7-8962-b23e00c896ac
     GCP_BW_ID=62511176-c5bd-4f9c-8de6-b1e700f570b1
 
     # ensure we have latest passwords
@@ -82,6 +83,7 @@ elif test "$SLACK_BOT_TOKEN" = "changeme" -o -z "$SLACK_BOT_TOKEN" -o "$FORCE_UP
     ensure_value SLACK_APP_TOKEN "$(bw get password $SLACK_APP_TOKEN_BW_ID)" "$ENV_FILE"
     ensure_value SLACK_SIGNING_SECRET "$(bw get password $SLACK_SIGNING_SECRET_BW_ID)" "$ENV_FILE"
     ensure_value DATA_TEAM_GITHUB_API_TOKEN "$(bw get password $DATA_TEAM_GITHUB_API_TOKEN_BW_ID)" "$ENV_FILE"
+    ensure_value CODESPACES_GITHUB_API_TOKEN "$(bw get password $CODESPACES_GITHUB_API_TOKEN_BW_ID)" "$ENV_FILE"
 
     # create/update the GCP credentials file with the JSON retrieved from bitwarden
     echo "Writing credentials to $GCP_CREDENTIALS_PATH"
