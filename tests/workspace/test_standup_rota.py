0cfe16cbd5446fc3d9c899ef4deee1fbe01ba251
Lucy Bridges <lucy.bridges@thedatalab.org>
2024-12-16 16:34:59 +0000

    Daily standup rota

--- tests/workspace/test_standup_rota.py
+++ tests/workspace/test_standup_rota.py
@@ -1,6 +1,7 @@
 import json
+from datetime import datetime
 
-from workspace.standup.jobs import weekly_rota
+from workspace.standup.jobs import daily_rota, get_weekday_date, weekly_rota
 
 
 def test_weekly_rota_odd_week(freezer):
@@ -35,3 +36,54 @@ def test_weekly_rota_even_week(freezer):
             "type": "section",
         },
     ]
+
+
+def test_daily_rota_odd_week(freezer):
+    freezer.move_to("2024-12-16")
+    assert json.loads(daily_rota("monday")) == [
+        {
+            "text": {"text": "Team Rex stand up", "type": "plain_text"},
+            "type": "header",
+        },
+        {
+            "text": {
+                "text": "Monday: Jon (backup: Mike)",
+                "type": "mrkdwn",
+            },
+            "type": "section",
+        },
+    ]
+
+
+def test_daily_rota_even_week(freezer):
+    freezer.move_to("2024-12-09")
+    assert json.loads(daily_rota("wednesday")) == [
+        {
+            "text": {"text": "Team Rex stand up", "type": "plain_text"},
+            "type": "header",
+        },
+        {
+            "text": {
+                "text": "Wednesday: Mary (backup: Steve)",
+                "type": "mrkdwn",
+            },
+            "type": "section",
+        },
+    ]
+
+
+def test_get_weekday_date(freezer):
+    freezer.move_to("2024-12-10")  # Tuesday
+    assert get_weekday_date("monday") == datetime(2024, 12, 16).date()
+    assert get_weekday_date("wednesday") == datetime(2024, 12, 11).date()
+    assert get_weekday_date("friday") == datetime(2024, 12, 13).date()
+
+    freezer.move_to("2024-12-12")  # Thursday
+    assert get_weekday_date("monday") == datetime(2024, 12, 16).date()
+    assert get_weekday_date("wednesday") == datetime(2024, 12, 18).date()
+    assert get_weekday_date("friday") == datetime(2024, 12, 13).date()
+
+    freezer.move_to("2024-12-15")  # Sunday
+    assert get_weekday_date("monday") == datetime(2024, 12, 16).date()
+    assert get_weekday_date("wednesday") == datetime(2024, 12, 18).date()
+    assert get_weekday_date("friday") == datetime(2024, 12, 20).date()
--- workspace/standup/jobs.py
+++ workspace/standup/jobs.py
@@ -1,14 +1,27 @@
 import json
-from datetime import date
+from datetime import date, datetime, timedelta
 
 from workspace.utils import blocks
 
 
 pairs = {
-    "Monday": ["Mike", "Jon"],
-    "Wednesday": ["Mary", "Steve"],
-    "Friday": ["Thomas", "Katie"],
+    "monday": ["Mike", "Jon"],
+    "wednesday": ["Mary", "Steve"],
+    "friday": ["Thomas", "Katie"],
 }
+
+
+def get_weekday_date(day_of_week):
+    if day_of_week == "monday":
+        weekday = 0
+    elif day_of_week == "wednesday":
+        weekday = 2
+    elif day_of_week == "friday":
+        weekday = 4
+
+    offset_days = (weekday - date.weekday(datetime.today())) % 7
+    rota_date = datetime.today() + timedelta(offset_days)
+    return rota_date.date()
 
 
 def is_even_week(rota_date):
@@ -23,14 +36,34 @@ def weekly_rota():
     header = "Team Rex stand ups this week"
     days = "\n".join(
         [
-            f"Monday: {pairs['Monday'][primary]} (backup: {pairs['Monday'][secondary]})",
-            f"Wednesday: {pairs['Wednesday'][primary]} (backup: {pairs['Wednesday'][secondary]})",
-            f"Friday: {pairs['Friday'][primary]} (backup: {pairs['Friday'][secondary]})",
+            f"Monday: {pairs['monday'][primary]} (backup: {pairs['monday'][secondary]})",
+            f"Wednesday: {pairs['wednesday'][primary]} (backup: {pairs['wednesday'][secondary]})",
+            f"Friday: {pairs['friday'][primary]} (backup: {pairs['friday'][secondary]})",
         ]
     )
 
     return json.dumps(blocks.get_basic_header_and_text_blocks(header, days))
 
 
+def daily_rota(day_of_week):
+    rota_date = get_weekday_date(day_of_week)
+
+    primary = is_even_week(rota_date)
+    secondary = 0 if primary else 1
+    header = "Team Rex stand up"
+    body = f"{day_of_week.title()}: {pairs[day_of_week][primary]} (backup: {pairs[day_of_week][secondary]})"
+
+    return json.dumps(blocks.get_basic_header_and_text_blocks(header, body))
+
+
 if __name__ == "__main__":
-    print(weekly_rota())
+    try:
+        print(weekly_rota())
+    except Exception as e:
+        print(
+            json.dumps(
+                blocks.get_basic_header_and_text_blocks(
+                    header_text="An error occurred", texts=str(e)
+                )
+            )
+        )
