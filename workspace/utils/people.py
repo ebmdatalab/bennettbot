d801f19f2c6db173361e0965836af785092eb37f
Jon Massey <jon.massey@thedatalab.org>
2025-01-03 17:44:27 +0000

    Define Team REX constant
    To allow for selection of static rota for this team.
    
    Refactor dict of github-username to slack-username
    into an enum of staff for ease of referring to
    staff members by real-world name rather than
    github username.

--- workspace/utils/people.py
+++ workspace/utils/people.py
@@ -1,27 +1,55 @@
-def get_slack_username(github_username):
-    """Get a dict mapping GitHub username to Slack username."""
-    # Find a user's username by right clicking on their name in the Slack app and clicking "Copy link"
-    user_id = {
-        "CLStables": "U036A6LTR7D",
-        "Jongmassey": "U023ZG5H24R",
-        "KatieB5": "U07KUKWBGKV",
-        "Mary-Anya": "U07LKQ06Q8L",
-        "Providence-o": "U07AGDM6ZJN",
-        "StevenMaude": "U01TJP3CG76",
-        "alarthast": "U07KX6L3CMA",
-        "benbc": "U01SPCP06Q1",
-        "bloodearnest": "U01AMBZUT47",
-        "eli-miriam": "U07LHEJ9TS4",
-        "evansd": "UAXE5V4RG",
-        "iaindillingham": "U01S6BLGK28",
-        "inglesp": "U4N1YPAP7",
-        "lucyb": "U035FT48KEK",
-        "madwort": "U019R5FJ7G8",
-        "milanwiedemann": "U02GPV8NNU9",
-        "mikerkelly": "U07KKL4PJJY",
-        "rebkwok": "U01SP5JLBFD",
-        "remlapmot": "U07L0L0SS6M",
-        "rw251": "U07QEMHUUMD",
-        "tomodwyer": "U01UQ0T2M7V",
-    }.get(github_username) or github_username
-    return f"<@{user_id}>"
+from collections import namedtuple
+from enum import Enum
+
+
+Person = namedtuple("Person", ["github_username", "slack_username"])
+
+
+class People(Enum):
+    """Tech team members' GitHub and Slack usernames."""
+
+    # Find a Slack user's username by right-clicking on their name in the Slack app and clicking "Copy link".
+
+    ALICE = Person("alarthast", "U07KX6L3CMA")
+    BECKY = Person("rebkwok", "U01SP5JLBFD")
+    BEN_BC = Person("benbc", "U01SPCP06Q1")
+    CATHERINE = Person("CLStables", "U036A6LTR7D")
+    DAVE = Person("evansd", "UAXE5V4RG")
+    ELI = Person("eli-miriam", "U07LHEJ9TS4")
+    IAIN = Person("iaindillingham", "U01S6BLGK28")
+    JON = Person("Jongmassey", "U023ZG5H24R")
+    KATIE = Person("KatieB5", "U07KUKWBGKV")
+    LUCY = Person("lucyb", "U035FT48KEK")
+    MARY = Person("Mary-Anya", "U07LKQ06Q8L")
+    MILAN = Person("milanwiedemann", "U02GPV8NNU9")
+    MIKE = Person("mikerkelly", "U07KKL4PJJY")
+    PETER = Person("inglesp", "U4N1YPAP7")
+    PROVIDENCE = Person("Providence-o", "U07AGDM6ZJN")
+    RICHARD = Person("rw251", "U07QEMHUUMD")
+    SIMON = Person("bloodearnest", "U01AMBZUT47")
+    STEVE = Person("StevenMaude", "U01TJP3CG76")
+    THOMAS = Person("tomodwyer", "U01UQ0T2M7V")
+    TOM_P = Person("remlapmot", "U07L0L0SS6M")
+    TOM_W = Person("madwort", "U019R5FJ7G8")
+
+
+TEAM_REX = {
+    People.JON,
+    People.KATIE,
+    People.LUCY,
+    People.STEVE,
+    People.MIKE,
+    People.THOMAS,
+}
+
+
+def get_slack_username(github_username: str) -> str:
+    person = (
+        [p.value for p in People if p.value.github_username == github_username]
+        or [Person(github_username=None, slack_username=github_username)]
+    )[0]
+    return get_formatted_slack_username(person)
+
+
+def get_formatted_slack_username(person: Person) -> str:
+    return f"<@{person.slack_username}>"
