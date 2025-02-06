a0bef130001e000b7d912ba2a71cce667f453b58
Mike Kelly <mike.kelly@thedatalab.org>
2025-02-06 14:27:43 +0000

    Populate `human_readable` automatically.
    `PersonCollection.__init__()` populates `human_readable` based on attribute
    value if not set explicitly. This occurs when the class definition is read.
    This avoids the need to explicitly specify it for most people, when it's mostly
    duplicative of the attribute value, for example MIKE => Mike.
    
    Cases where the attempted conversion doesn't give the result we want can still
    be manually specified in the definition of `People`. For example, with "BEN_BC"
    it's not easy to determine programatically that all of the second word should
    be capitalized, so we specify explicitly as "Ben BC".

--- workspace/utils/people.py
+++ workspace/utils/people.py
@@ -1,55 +1,70 @@
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass(order=True)
 class Person:
-    human_readable: str
     github_username: str
     slack_username: str
+    # Optional because this is mostly set in PersonCollection.__init__().
+    human_readable: Optional[str] = ""
 
     @property
     def formatted_slack_username(self) -> str:
         return f"<@{self.slack_username}>"
 
 
-class PersonIterator(type):
-    """Metaclass to iterate on Person instances in class attributes.
+class PersonCollection(type):
+    """Metaclass for collections of Person instances."""
 
-    This is to allow `for person in People` or similar to work."""
+    def __init__(cls, name, bases, namespace):
+        people_items = [
+            item for item in namespace.items() if isinstance(item[1], Person)
+        ]
+        for attribute_name, person in people_items:
+            # Populate human_readable based on attribute value if not set explicitly.
+            if not person.human_readable:
+                person.human_readable = attribute_name.title().replace("_", " ")
+
+        super().__init__(name, bases, namespace)
 
     def __iter__(cls):
+        # Enable iteration by `for person in People` or similar to work.
         return iter(
             sorted(value for value in vars(cls).values() if isinstance(value, Person))
         )
 
 
-class People(metaclass=PersonIterator):
+class People(metaclass=PersonCollection):
     """Tech team members' GitHub and Slack usernames."""
 
     # Find a Slack user's username by right-clicking on their name in the Slack
     # app and clicking "Copy link".
 
-    ALICE = Person("Alice", "alarthast", "U07KX6L3CMA")
-    BECKY = Person("Becky", "rebkwok", "U01SP5JLBFD")
-    BEN_BC = Person("Ben BC", "benbc", "U01SPCP06Q1")
-    CATHERINE = Person("Catherine", "CLStables", "U036A6LTR7D")
-    DAVE = Person("Dave", "evansd", "UAXE5V4RG")
-    ELI = Person("Eli", "eli-miriam", "U07LHEJ9TS4")
-    IAIN = Person("Iain", "iaindillingham", "U01S6BLGK28")
-    JON = Person("Jon", "Jongmassey", "U023ZG5H24R")
-    KATIE = Person("Katie", "KatieB5", "U07KUKWBGKV")
-    LUCY = Person("Lucy", "lucyb", "U035FT48KEK")
-    MARY = Person("Mary", "Mary-Anya", "U07LKQ06Q8L")
-    MILAN = Person("Milan", "milanwiedemann", "U02GPV8NNU9")
-    MIKE = Person("Mike", "mikerkelly", "U07KKL4PJJY")
-    PETER = Person("Peter", "inglesp", "U4N1YPAP7")
-    PROVIDENCE = Person("Providence", "Providence-o", "U07AGDM6ZJN")
-    RICHARD = Person("Richard", "rw251", "U07QEMHUUMD")
-    SIMON = Person("Simon", "bloodearnest", "U01AMBZUT47")
-    STEVE = Person("Steve", "StevenMaude", "U01TJP3CG76")
-    THOMAS = Person("Thomas", "tomodwyer", "U01UQ0T2M7V")
-    TOM_P = Person("Tom P", "remlapmot", "U07L0L0SS6M")
-    TOM_W = Person("Tom W", "madwort", "U019R5FJ7G8")
+    # human_readable is mostly set by PersonCollection.__init__() but can be
+    # overriden as the third argument if required. This avoids the need to
+    # explicitly specify it for most people.
+    ALICE = Person("alarthast", "U07KX6L3CMA")
+    BECKY = Person("rebkwok", "U01SP5JLBFD")
+    BEN_BC = Person("benbc", "U01SPCP06Q1", "Ben BC")
+    CATHERINE = Person("CLStables", "U036A6LTR7D")
+    DAVE = Person("evansd", "UAXE5V4RG")
+    ELI = Person("eli-miriam", "U07LHEJ9TS4")
+    IAIN = Person("iaindillingham", "U01S6BLGK28")
+    JON = Person("Jongmassey", "U023ZG5H24R")
+    KATIE = Person("KatieB5", "U07KUKWBGKV")
+    LUCY = Person("lucyb", "U035FT48KEK")
+    MARY = Person("Mary-Anya", "U07LKQ06Q8L")
+    MILAN = Person("milanwiedemann", "U02GPV8NNU9")
+    MIKE = Person("mikerkelly", "U07KKL4PJJY")
+    PETER = Person("inglesp", "U4N1YPAP7")
+    PROVIDENCE = Person("Providence-o", "U07AGDM6ZJN")
+    RICHARD = Person("rw251", "U07QEMHUUMD")
+    SIMON = Person("bloodearnest", "U01AMBZUT47")
+    STEVE = Person("StevenMaude", "U01TJP3CG76")
+    THOMAS = Person("tomodwyer", "U01UQ0T2M7V")
+    TOM_P = Person("remlapmot", "U07L0L0SS6M")
+    TOM_W = Person("madwort", "U019R5FJ7G8")
 
     # Dict mapping github_username to People, constructed lazily and cached the
     # first time it's needed. Can't be built in the class definition as we
