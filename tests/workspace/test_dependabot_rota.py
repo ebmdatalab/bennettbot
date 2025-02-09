e431123adabb6b6662dcd31d9a98675c45916974
Mike Kelly <mike.kelly@thedatalab.org>
2025-02-09 14:21:43 +0000

    Break very long str over multiple lines.

--- tests/workspace/test_dependabot_rota.py
+++ tests/workspace/test_dependabot_rota.py
@@ -6,7 +6,16 @@ from workspace.utils.people import People
 
 TEAM_REX = [People.JON, People.LUCY, People.KATIE]
 
-EXTRA_TEXT = "\nReview <https://github.com/opensafely-core/job-server/pulls|job-server>, <https://github.com/opensafely-core/opencodelists/pulls|opencodelists>, <https://github.com/ebmdatalab/metrics/pulls|metrics>, <https://github.com/opensafely-core/reports/pulls|reports>, <https://github.com/opensafely-core/actions-registry/pulls|actions-registry> and <https://github.com/opensafely-core/research-template-docker/pulls|research-template-docker> repos and merge any outstanding non-NPM Dependabot/update-dependencies-action PRs.\nReview Thomas' PRs for NPM updates.\n"
+EXTRA_TEXT = (
+    "\nReview <https://github.com/opensafely-core/job-server/pulls|job-server>, "
+    "<https://github.com/opensafely-core/opencodelists/pulls|opencodelists>, "
+    "<https://github.com/ebmdatalab/metrics/pulls|metrics>, "
+    "<https://github.com/opensafely-core/reports/pulls|reports>, "
+    "<https://github.com/opensafely-core/actions-registry/pulls|actions-registry> "
+    "and <https://github.com/opensafely-core/research-template-docker/pulls|research-template-docker> "
+    "repos and merge any outstanding non-NPM Dependabot/update-dependencies-action PRs."
+    "\nReview Thomas' PRs for NPM updates.\n"
+)
 
 
 def test_rota_report_on_monday(freezer, monkeypatch):
